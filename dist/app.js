9d8ffc42e692b1c14a5ebefda5c2567e0b76e8b3
yann <yann.boisselier@gmail.com>
2016-11-09 23:08:53 +0100

    fix de la configuration du calculateur

--- dist/app.js
+++ dist/app.js
@@ -39108,6 +39108,11 @@
 	 * http://service.cipav-retraite.fr/cipav/rubriquemax04-montant-des-charges.htm
 	 * http://service.cipav-retraite.fr/cipav/articlemax1-votre-protection-sociale-99.htm
 	 * http://www.cnavpl.fr/les-chiffres-cles/principaux-parametres-du-regime-de-base/principaux-parametres-variables-du-regime-de-base/
+	 *
+	 * https://www.rsi.fr/cotisations/professions-liberales/presentation-des-cotisations.html
+	 * Le RSI gère uniquement votre protection santé maladie-maternité.
+	 * la retraite et l'invalidité décès sont assurées par la CNAVPL ou la CNBF
+	 * les cotisations d'allocations familiales, les contributions sociales (CSG/CRDS) et les contributions à la formation professionnelle sont à verser à l'Urssaf
 	 */
 	angular.module('calculator').service('calculatorConfig', function(){
 	
@@ -39130,22 +39135,9 @@
 	    reduite: 5.5
 	  };
 	
-	  // données concernant les organismes
-	  parametres.organismes = {
-	    urssaf: {
-	      label: "URSSAF"
-	    },
-	    rsi: {
-	      label: 'RSI'
-	    },
-	    cipav: {
-	      label: 'CIPAV'
-	    }
-	  };
-	
 	  // URSSAF : MALADIE-MATERNITE
 	  parametres.charges.maladiesMaternite = {
-	    organisme:'urssaf',
+	    organisme:'URSSAF',
 	    label:'Maladie-maternité',
 	    commentaire:'Base de calcul : totalité des revenus professionnels',
 	    type_tranches: 'tranche_exclusive',
@@ -39159,7 +39151,7 @@
 	
 	  // URSSAF : ALLOCATIONS FAMILIALES
 	  parametres.charges.allocationsFamiliales = {
-	    organisme:'urssaf',
+	    organisme:'URSSAF',
 	    label:'Allocations familiales',
 	    commentaire:"Pour les revenus compris entre 42 478 € et 54 062 €, taux progressif : entre 2,15 % et 5,25 %. Faute de détails, le calculateur passe à 5.25 dès qu'on dépasse 42 478 €",
 	    type_tranches: 'tranche_exclusive',
@@ -39179,7 +39171,7 @@
 	
 	  // URSSAF : CGS-CRDS
 	  parametres.charges.cgsCrds = {
-	    organisme:'urssaf',
+	    organisme:'URSSAF',
 	    label:'CGS-CRDS',
 	    commentaire:"Base de calcul : 	Totalité du revenu de l’activité non salariée + cotisations sociales obligatoires",
 	    type_tranches: 'exclusive',
@@ -39193,7 +39185,7 @@
 	
 	  // URSSAF : FORMATION PROFESSIONNELLE
 	  parametres.charges.formationProfessionnelle = {
-	    organisme: 'urssaf',
+	    organisme: 'URSSAF',
 	    label: 'Formation professionnelle',
 	    commentaire: "Base de calcul : Sur la base de 38 616 . Cotisation à verser en 2016. Si votre conjoint a opté pour le statut de conjoint collaborateur, le taux est de 0,34 %",
 	    type_tranches: 'exclusive',
@@ -39209,13 +39201,10 @@
 	  // http://service.cipav-retraite.fr/cipav/article-33-recapitulatif-des-options-de-montantmax04.htm
 	  parametres.charges.assuranceVieillesseBase = {
 	    label: 'Retraite de base',
+	    organisme: 'CIPAV',
 	    type_tranches: 'exclusive',
 	    description: "Retraite de base CNAVPL",
-	    revenusNonConnus: 3324 + 6137,
-	    forfait: {
-	      plafond: 9171,
-	      total: 190
-	    },
+	    commentaire: "En cas de revenus non connus : 3 178 € (maximum de la tranche 1) ; 3 611 € (maximum de la tranche 2)",
 	    tranches: [
 	      // sous 4441, montant forfaitaire
 	      {
@@ -39233,12 +39222,10 @@
 	    ]
 	  };
 	
-	  parametres.charges.prevoyance = {
-	    commentaire: "76, 228, ou 380 euros suivant la classe choisie"
-	  };
-	
+	  // IMPOT
 	  parametres.charges.impotSurLesSocietes = {
 	    label: 'Impot sur les sociétés',
+	    organisme: "Impots",
 	    type_tranches: 'cumulatives',
 	    tranches:[
 	      {
@@ -39252,59 +39239,88 @@
 	    ]
 	  };
 	
-	  // Assurance vieillesse complémentaire (obligatoire)
+	  // CIPAV: Assurance vieillesse complémentaire (obligatoire)
 	  // http://service.cipav-retraite.fr/cipav/article-33-recapitulatif-des-options-de-montantmax04.htm
 	  parametres.charges.assuranceVieillesseComplementaire = {
 	    label : 'Retraite complémentaire',
+	    organisme: 'CIPAV',
 	    type : "exclusive",
 	    tranches : [
 	      {
 	        nom : 'A',
-	        plafond : 26420,
-	        montant_forfaitaire : 1198,
+	        plafond : 26580,
+	        montant_forfaitaire : 1214,
 	        points_retraite : 36
 	      },
 	      {
 	        nom : 'B',
-	        plafond : 48890,
-	        montant_forfaitaire : 2395,
+	        plafond : 49280,
+	        montant_forfaitaire : 2427,
 	        points_retraite : 72
 	      },
 	      {
 	        nom : 'C',
-	        plafond : 57500,
-	        montant_forfaitaire : 3593,
+	        plafond : 57850,
+	        montant_forfaitaire : 3641,
 	        points_retraite : 108
 	      },
 	      {
 	        nom : 'D',
-	        plafond : 66000,
-	        montant_forfaitaire : 5989,
+	        plafond : 66400,
+	        montant_forfaitaire : 6068,
 	        points_retraite : 180
 	      },
 	      {
 	        nom : 'E',
-	        plafond : 82260,
-	        montant_forfaitaire : 8394,
+	        plafond : 83060,
+	        montant_forfaitaire : 8495,
 	        points_retraite :  252
 	      },
 	      {
 	        nom : 'F',
-	        plafond : 102560,
-	        montant_forfaitaire : 13175,
+	        plafond : 103180,
+	        montant_forfaitaire : 13349,
 	        points_retraite : 396
 	      },
 	      {
 	        nom : 'G',
-	        plafond : 122560,
-	        montant_forfaitaire : 14373,
+	        plafond : 123300,
+	        montant_forfaitaire : 14563,
 	        points_retraite : 432
 	      },
 	      {
 	        nom : 'H',
 	        plafond : max,
-	        montant_forfaitaire : 15570,
+	        montant_forfaitaire : 15776,
 	        points_retraite : 468
+	      }
+	    ]
+	  };
+	
+	  // Réduction assurance vieillesse complémentaire
+	  parametres.charges.AssuranceVieillesseComplementaireReduction = {
+	    label: "Réduction assurance vieillesse complémentaire",
+	    type: "exclusive",
+	    tranches: [
+	      {
+	        plafond : 5792,
+	        taux : 100,
+	        points_retraite:0
+	      },
+	      {
+	        plafond : 11585,
+	        taux : 75,
+	        points_retraite:9
+	      },
+	      {
+	        plafond :  17377,
+	        taux :50,
+	        points_retraite:18
+	      },
+	      {
+	        plafond : 23170,
+	        taux : 25,
+	        points_retraite:27
 	      }
 	    ]
 	  };
@@ -39324,30 +39340,6 @@
 	        montant_forfaitaire: 380
 	      }
 	    }
-	  };
-	
-	  // Réduction assurance vieillesse complémentaire
-	  parametres.charges.AssuranceVieillesseComplementaireReduction = {
-	    label: "Réduction assurance vieillesse complémentaire",
-	    type: "exclusive",
-	    tranches: [
-	      {
-	        plafond : 5632,
-	        taux : 100
-	      },
-	      {
-	        plafond : 11264,
-	        taux : 0.75
-	      },
-	      {
-	        plafond :  16897,
-	        taux : 0.50
-	      },
-	      {
-	        plafond : 22529,
-	        taux : 0.25
-	      }
-	    ]
 	  };
 	
 	  // les professions libérales ne cotisent pas pour les indemnités journalières
